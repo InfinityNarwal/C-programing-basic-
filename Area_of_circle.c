ad8144982c93986e0040a8479d3bc877dc07d44d
IninityNarwal <92997061+InfinityNarwal@users.noreply.github.com>
2021-12-21 14:43:55 +0530

    Add files via upload

---
+++ Area_of_circle.c
@@ -0,0 +1,10 @@
+//Area of circle
+#include<stdio.h>
+
+int main(){
+    float pie = 3.14, r;
+    printf("please enter the radius of circle");
+    scanf("%f", r);
+    printf("Area of circle is %f", pie*r*r);
+
+}
---
+++ celciustodegree.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    float c, t;
+    printf("please enter the temp. in celcius  \n");
+    scanf("%f", &c);
+    printf("the temp. in faranite is %f", c*1.8+32);
+}
